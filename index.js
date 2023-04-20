f7408df9f26f5c8f87befea28e8bac5da841eb6c
devzippel <110544211+devzippel@users.noreply.github.com>
2023-04-19 22:36:27 -0300

    SetInterval 15 minutos

--- index.js
+++ index.js
@@ -11,41 +11,58 @@ app.use(express.json());
 
 const MORALIS_API_KEY = process.env.MORALIS_API_KEY;
 
-app.get("/getethprice", async (req, res) => {
-    try {
-        const response = await Moralis.EvmApi.token.getTokenPrice({
-            address: "0x14bb7a637fAb7Ef189Ddb052153239cf31892D8c",
-            chain: "0x38",
-        });
+async function getEthPrice() {
+  try {
+    const response = await Moralis.EvmApi.token.getTokenPrice({
+      address: "0x14bb7a637fAb7Ef189Ddb052153239cf31892D8c",
+      chain: "0x38",
+    });
 
-        return res.status(200).json(response);
-    } catch (e) {
-        console.log(`Somthing went wrong ${e}`);
-        return res.status(400).json();
-    }
+    console.log(`ETH price: ${response.price}`);
+
+  } catch (e) {
+    console.log(`Something went wrong: ${e}`);
+  }
+}
+
+app.get("/getethprice", async (req, res) => {
+  try {
+    const response = await Moralis.EvmApi.token.getTokenPrice({
+      address: "0x14bb7a637fAb7Ef189Ddb052153239cf31892D8c",
+      chain: "0x38",
+    });
+
+    return res.status(200).json(response);
+  } catch (e) {
+    console.log(`Something went wrong: ${e}`);
+    return res.status(400).json();
+  }
 });
 
 app.get("/address", async (req, res) => {
-    try {
-        const { query } = req;
-        const chain = "0x38";
+  try {
+    const { query } = req;
+    const chain = "0x38";
 
-        const response = await Moralis.EvmApi.transaction.getWalletTransactions({
-            address: query.address,
-            chain: 0x38,
-        });
+    const response = await Moralis.EvmApi.transaction.getWalletTransactions({
+      address: query.address,
+      chain: 0x38,
+    });
 
-        return res.status(200).json(response);
-    } catch (e) {
-        console.log(`Something went wrong ${e}`);
-        return res.status(400).json();
-    }
+    return res.status(200).json(response);
+  } catch (e) {
+    console.log(`Something went wrong: ${e}`);
+    return res.status(400).json();
+  }
 });
 
 Moralis.start({
-    apiKey: MORALIS_API_KEY,
+  apiKey: MORALIS_API_KEY,
 }).then(() => {
-    app.listen(port, '0.0.0.0', () => {
-        console.log(`Listening for API Calls`);
-    });
+  app.listen(port, "0.0.0.0", () => {
+    console.log(`Listening for API Calls`);
+
+    // Call the getEthPrice function every 15 minutes
+    setInterval(getEthPrice, 15 * 60 * 1000);
+  });
 });
